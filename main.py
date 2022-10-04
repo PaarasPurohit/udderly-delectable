c5526557166711a848861007134341f9af820508
kkcbal <tabbed.out3321@gmail.com>
2022-10-04 15:44:14 -0700

    about page

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ def index():
 def stub():
     return render_template("stub.html")
 
+@app.route('/about/')
+def about():
+    return render_template("about.html")
+
 # this runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True)
